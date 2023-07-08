a59f91ffab11899749947e1427987a2480625440
Casey <leptone55@gmail.com>
2023-07-08 16:25:53 +0200

    conneciton working

--- app/_providers/WagmiProvider.tsx
+++ app/_providers/WagmiProvider.tsx
@@ -20,21 +20,12 @@ const wagmiConfig = createConfig({
   publicClient,
 })
 
-function Something() {
-  return (
-    <div>
-      <h1>Something</h1>
-    </div>
-  )
-}
-
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const WagmiProvider = ({ children }: WagmiProviderType) => {
   return (
     <>
       <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-      <Something />
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import Image from 'next/image'
 import Test from '@/app/components/test'
+import { Web3Button } from '@web3modal/react'
 
 export default function Home() {
   return (
     <main className=''>
-      <Test />
+      <Web3Button />
     </main>
   )
 }
