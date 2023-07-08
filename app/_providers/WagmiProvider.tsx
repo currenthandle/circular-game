23f96e1f6fef42bc6fb04d29c5c9e11f54c98f6e
Casey <leptone55@gmail.com>
2023-07-08 16:35:00 +0200

    update chains

--- app/_providers/WagmiProvider.tsx
+++ app/_providers/WagmiProvider.tsx
@@ -3,19 +3,18 @@ import React from 'react'
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { polygonMumbai } from 'wagmi/chains'
+import { mainnet, gnosis } from 'wagmi/chains'
 
 type WagmiProviderType = {
   children: React.ReactNode
 }
 
-const chains = [polygonMumbai]
+const chains = [mainnet, gnosis]
 const projectId = process.env.NEXT_PUBLIC_W3C_PID
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
   autoConnect: true,
-  //   connectors: w3mConnectors({ projectId, version: 2, chains }),
   connectors: w3mConnectors({ projectId, chains }),
   publicClient,
 })
